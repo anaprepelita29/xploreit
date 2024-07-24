a707d259976e2420b3555129ba0ce0ff15bd6fe7
ana <anaprepelita29@gmail.com>
2024-07-24 18:10:33 +0300

    test comm

--- Xploreit/src/App.vue
+++ Xploreit/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+//azi maine
 import { RouterLink, RouterView } from 'vue-router'
 </script>
 
@@ -6,6 +7,6 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped>
+<style scoped> 
 
 </style>
