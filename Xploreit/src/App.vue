625dbd318246427f6f245c73957fad6cb75cb7ec
iosipi <iosipirina79@gmail.com>
2024-07-26 11:18:48 +0300

    verific header

--- Xploreit/src/App.vue
+++ Xploreit/src/App.vue
@@ -7,9 +7,24 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   
+  <header>
+    <nav>
+      <div class="login">
+        <a href="#"><img src="../public/imagini/logo.png.png"></a>
+        <ul>
+          <li><a href="sign.html">Sign in</a></li>
+          <li><a href="login.html">Login</a></li>
+        </ul>
+        <ul>
+          <li>RO</li>
+          <li><button type="menu"> <img src=""></button></li>
+        </ul>
+      </div>
+
+    </nav>
+  </header>
+
   <RouterView />
 </template>
 
-<style scoped> 
-
-</style>
+<style scoped></style>
